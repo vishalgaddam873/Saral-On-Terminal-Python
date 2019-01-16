3423b17a51a67e3d55efb100aa387335c705a096
Rock's Vishu <you@example.com>
2019-01-16 13:38:09 +0530

    Adding new features to api

--- saral_api.py
+++ saral_api.py
@@ -1,134 +1,153 @@
-import requests,json,os,pprint
+import requests
+import json
+import os
+import pprint
 
-url = "http://saral.navgurukul.org/api/courses"
+# This is our main url of api 
+saral_api_url = "http://saral.navgurukul.org/api/courses"
 
+def user():
+	choice_user = int(input("Enetr Index number of Course: ")) 
+	return choice_user
+
+# Here we grab the slug of child_exercises.
+def slug_api(user_input,slug):
+	path = 'request_data/exercise_slug/exercise_' + str(user_input)
+	os.mkdir(path)
+	for s in slug:
+		exercises_url = saral_api_url +"/" + str(user_input) + '/exercise'
+		slug_url = exercises_url + '/getBySlug?slug=' + s
+		slug_requests = requests.get(slug_url)
+		slug1 = ''
+		for i in s:
+			if '/' not in i:
+				slug1 += i
+			else:
+				slug1 +='_'
+		file_name = slug1 + '.json'
+		
+		with open( path + "/" + file_name ,'w') as file3:
+			json.dump(slug_requests.text,file3)
+
+# Here we read the child exercises of saral_api_url
+def read_child_exercise(user_input):
+	file_name = 'request_data/course_exe/exercises_'+ str(user_input) + '.json'
+	with open(file_name,'r') as file2:
+		raw = file2.read() # the type of raw is unicode_type
+		unicode_s = json.loads(raw) # The type of unicode_s is also unicode_type of string_type.
+		text = json.loads(unicode_s) # The type of text is dic_type
+		data = text['data']
+		slugs = []
+		for i in (data):
+			parent_exe = i['name']
+			child_exe = i['childExercises']
+			child_slug = i['slug']
+			slugs.append(child_slug)
+			index = 1
+			for j in child_exe:
+				sub_exe = j['name']
+				if index == 1:
+					print('---------' + parent_exe + '---------')
+					print('     ' + str(index) + '.' + sub_exe)
+				else:
+					print('     '+ str(index) + '.' + sub_exe)
+				index+=1
+	return slugs
+
+# Here we grab the child exercises from sarl_api_url
 def child_exe(user_input):
-
 	file_name = 'request_data/course_exe/exercises_'+ str(user_input) + '.json'
 	debug = False
 	text = None
 	if os.path.exists(file_name):
 		debug =  True
-		with open(file_name,'r') as file2:
-			raw = file2.read() # the type of raw is unicode_type
-			s = json.loads(raw) # The type of s is also unicode_type
-			text = json.loads(s) # The type of dic is dic_type
-			data = text['data']
-			for i in (data):
-				parent_exe = i['name']
-				child_exe = i['childExercises']
-				index = 1
-				for j in child_exe:
-					sub_exe = j['name']
-					if index == 1:
-						print('---------' + parent_exe + '---------')
-						print('     ' + str(index) + '.' + sub_exe)
-					else:
-						print('     '+ str(index) + '.' + sub_exe)
-					index+=1
-
-
+		read_child_exercise(user_input)
 
 	if debug is False:
-		exercises_url = url +"/" + str(user_input) + '/exercises'
+		exercises_url = saral_api_url +"/" + str(user_input) + '/exercises'
 		api_2 = requests.get(exercises_url)
 		raw = json.dumps(api_2.text)
 		file1 = open(file_name,'w')
 		file1.write(raw)
 		file1.close()
 
-		with open(file_name,'r') as file2:
-				raw = file2.read() # the type of raw is unicode_type
-				s = json.loads(raw) # The type of s is also unicode_type
-				text = json.loads(s) # The type of dic is dic_type
-				data = text['data']
-				slugs = []
-				for i in (data):
-					parent_exe = i['name']
-					child_exe = i['childExercises']
-					child_slug = i['slug']
-					slugs.append(child_slug)
+		# The read_child_exercise() return the slugs list  
+		slugs = read_child_exercise(user_input)
+		# Here we call the function called slug_api and passes user_input & slugs as argument in this function
+		slug_api(user_input,slugs)
+		
 
-					index = 1
-					for j in child_exe:
-						sub_exe = j['name']
-						if index == 1:
-							print('---------' + parent_exe + '---------')
-							print('     ' + str(index) + '.' + sub_exe)
-						else:
-							print('     '+ str(index) + '.' + sub_exe)
-						index+=1
-		path = 'request_data/exercise_slug/exercise_' + str(user_input)
-		os.mkdir(path)
-		for slug in slugs:
-			exercises_url = url +"/" + str(user_input) + '/exercise'
-			slug_url = exercises_url + '/getBySlug?slug=' + slug
-			slug_requests = requests.get(slug_url)
-			a = ''
-			for i in slug:
-				if '/' not in i:
-					a += i
-				else:
-					a +='_'
-			slug1 = a
-			file_name = slug1 + '.json'
-			
-			with open( path + "/" + file_name ,'w') as file3:
-				json.dump(slug_requests.text,file3)
-				
+# Here we create some empty list
+user_choice_list = []	
+course_id_list = []
+course_dic = {}
+# Here We read the course.json file.
+def read_course():
+	file_name = 'request_data/course.json'
+	with open(file_name,'r') as file1:
+		raw = file1.read() # the type of raw is unicode_type
+		s = json.loads(raw) # The type of s is also unicode_type
+		text = json.loads(s) # The type of dic is dic_type
+		# dic = ast.literal_eval(s) # Here we are directly convert unicode_type to dic_type in json
+		course = text['availableCourses']
+		for i in range(len(course)):
+			course_id = course[i]['id']
+			course_id_list.append(course_id)
+			course_name = course[i]['name']
+			course_dic[course_id] = course_name
+			print(i+1,course_name)
+		user_choice = user()
+		user_choice_list.append(user_choice-1)
+		check = course_id_list[user_choice-1]
+		print("")
+		print(course_dic[check])
+		print("")
+		child_exe(check)			
 
-
-# This code for parent exercises.
-def request_api(url):
-	# Task 2
+# Here we create the file called course.json and also check the that file is exist or not.
+def request_api(api_url):
 	file_name = 'request_data/course.json'
 	debug = False
 	text = None
 	if os.path.exists(file_name):
 		debug = True
-		with open("request_data/course.json",'r') as file1:
-			raw = file1.read() # the type of raw is unicode_type
-			s = json.loads(raw) # The type of s is also unicode_type
-			text = json.loads(s) # The type of dic is dic_type
-			# dic = ast.literal_eval(s) # Here we are directly convert unicode_type to dic_type in json
-			course = text['availableCourses']
-			course_dic = {}
-			for i in range(len(course)):
-				course_id = course[i]['id']
-				course_name = course[i]['name']
-				course_dic[course_id] = course_name
-			print(course_dic)
-			user_input = int(input("Enetr Index number of Course: "))
-			print("")
-			print(course_dic[user_input])
-			print("")
-			child_exe(user_input)
+		read_course()
 
-				
 	if debug is False:
-		# Task 1
-		api_1 = requests.get(url)
+		api_1 = requests.get(api_url)
 		raw = json.dumps(api_1.text)
 		file1 = open(file_name,'w')
 		file1.write(raw)
 		file1.close()
 
-		# Task 2
-		with open("request_data/course.json",'r') as file1:
-			raw = file1.read() # the type of raw is unicode_type
-			s = json.loads(raw) # The type of s is also unicode_type
-			text = json.loads(s) # The type of dic is dic_type
-			# dic = ast.literal_eval(s) # Here we are directly convert unicode_type to dic_type in json
-			course = text['availableCourses']
-			course_dic = {}
-			for i in range(len(course)):
-				course_id = course[i]['id']
-				course_name = course[i]['name']
-				course_dic[course_id] = course_name
-			print(course_dic)
-			user_choice = int(input("Enetr id of course: "))
-			print("")
-			print(course_dic[user_choice])
-			print("")
-			child_exe(user_choice)
-request_api(url)
+		read_course()
+
+request_api(saral_api_url)
+
+
+while True:
+	print("")
+	message = ''' Go to Main Menu enter: up\n For previous course enter: p\n For next course enter: n'''
+	print(message)
+	print("")
+	choice = input("Enter your Choice: ").upper()
+	if choice == 'UP':
+		request_api(saral_api_url)
+	elif choice == 'P':
+		number = user_choice_list[-1]-1
+		if number >=0:
+			check = course_id_list[number]
+			print(course_dic[check])
+			child_exe(check)
+		else:
+			print("Course is not available")
+	elif choice == 'N':
+		number = user_choice_list[-1]+1
+		if number >=0:
+			check = course_id_list[number]
+			print(course_dic[check])
+			child_exe(check)
+		else:
+			print("Course is not available")
+	else:
+		print("Enter the correct choice")
